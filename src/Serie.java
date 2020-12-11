b22e9eb3ba8b45ef4a4ff1bf5dc1c7681d72e9d4
VincentPiernot <vincentpiernot@gmail.com>
2020-12-11 11:45:32 +0100

    aaa

--- src/Serie.java
+++ src/Serie.java
@@ -3,6 +3,8 @@ public class Serie {
 
 	private int id;
 	private String nom, genre;
+	private static String sql = "CREATE TABLE Serie ( " + "ID INTEGER  AUTO_INCREMENT, " + "NOM varchar(40) NOT NULL, "
+			+ "GENRE varchar(40) NOT NULL, " + "PRIMARY KEY (ID))";
 
 	public Serie(String n, String g) {
 		id = -1;
